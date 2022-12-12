04006b3a44e1c2704b1ada9b75c2ffac7367a793
vnosinov <nosinov@gmail.com>
2022-12-12 16:27:47 +0200

    add abon_amount with error

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import xml.dom.minidom as minodom
 import xml.etree.cElementTree as ET
-import csv
 import pandas as pd
 import os
 from pprint import pprint
@@ -23,9 +21,19 @@ def parse_xml(filename):
 
         for contract_detail in member.findall('Contract/ContractDetail'):
             contract_id = contract_detail.find('ContractID')
+
+            amount_id = contract_detail.findall('Summary/SummaryRow/RowDetail/Summary/SummaryRow/RowDetail/Id')
+            amount = contract_detail.findall('Summary/SummaryRow/RowDetail/Summary/SummaryRow/RowDetail/Amount')
+
+            for i, e in zip(amount_id, amount):
+                if i.text == '1':
+
+                    abon_amount = float(e.text)
+
             for total_amount in contract_detail.findall('SubInvoiceAmount/AmountDetail/TotalAmount'):
-                pair_data = (billing_account, contract_id.text, float(total_amount.text))
+                pair_data = (billing_account, contract_id.text, float(total_amount.text), abon_amount)
                 data_dict[billing_account].append(pair_data)
+
     return data_dict
 
 
@@ -34,23 +42,22 @@ def stat_data(data_dict):
     for key in data_dict.keys():
         if key not in ['5210888', '5209074']:
             for i in data_dict[key]:
-                bill_acc, cont_id, total_am = i
+                bill_acc, cont_id, total_am, abon_am = i
+                # print(bill_acc, cont_id, total_am, abon_am)
+                my_list.append([bill_acc, cont_id, str(total_am).replace('.', ','), str(abon_am).replace('.', ',')])
 
-                my_list.append([bill_acc, cont_id, str(total_am).replace('.', ',')])
-
-    df = pd.DataFrame(my_list, columns=['bill', 'number', 'amount'])
-    writer = pd.ExcelWriter('output.xlsx')
-
-    df.to_excel(writer)  # save the excel
-    writer.save()
-    print('DataFrame is written successfully to Excel File.')
+    df = pd.DataFrame(my_list, columns=['bill', 'number', 'amount', 'abon'])
+    with pd.ExcelWriter('output.xlsx') as writer:
+        df.to_excel(writer)  # save the excel
+        print('DataFrame is written successfully to Excel File.')
 
 
 if __name__ == "__main__":
     PATH_TO_FILE = os.path.join(os.getcwd(), 'XML\\')
     list_of_files = get_files_xml(PATH_TO_FILE)
-    full_path = PATH_TO_FILE + list_of_files[0]
+    full_path = PATH_TO_FILE + list_of_files[1]
+    # print(full_path)
     xml_dict = parse_xml(full_path)
-
+    # pprint(xml_dict)
     stat_data(xml_dict)
 
